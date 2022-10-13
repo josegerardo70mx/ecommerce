3ae68f8edb8c9cf80dafa4481ae1a01a143f8a79
Mesach Venegas <mesach.venegas@hotmail.com>
2022-10-12 19:53:23 -0700

    remove: productos a el modulo stock

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-const items = [
-    {
-      id: 1,
-      name: 'Hoodies',
-      price: 14.00,
-      image: 'assets/images/featured1.png',
-      category: 'hoodies',
-      quantity: 10
-    },
-    {
-      id: 2,
-      name: 'Shirts',
-      price: 24.00,
-      image: 'assets/images/featured2.png',
-      category: 'shirts',
-      quantity: 15
-    },
-    {
-      id: 3,
-      name: 'Sweatshirts',
-      price: 24.00,
-      image: 'assets/images/featured3.png',
-      category: 'shirts',
-      quantity: 20
-    }
-  ]
 const menu = document.getElementById("menu")
 
 // Animacion de carga del website.
