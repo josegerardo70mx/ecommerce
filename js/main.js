d1ef4bbfe810392661835635f44239d6006eb363
Mesach Venegas <mesach.venegas@hotmail.com>
2022-10-12 19:04:06 -0700

    add: home

--- js/main.js
+++ js/main.js
@@ -26,12 +26,15 @@ const items = [
   ]
 const menu = document.getElementById("menu")
 
+// Animacion de carga del website.
 const loadComponent = () =>{
     const loader = document.getElementById("loader-container");
     setTimeout( () => {
         loader.classList.add("hide")
     }, 3000)
 }
+
+// abre el menu de navegación.
 const openMenu = () =>{
   const btnOpen = document.getElementById("menu-open")
   btnOpen.addEventListener("click", e => {
@@ -39,6 +42,7 @@ const openMenu = () =>{
   })
 }
 
+// cierra el menu de navegación.
 const closeMenu = () => {
   const btnClose = document.getElementById("menu-close")
   btnClose.addEventListener( "click", e => {
@@ -46,8 +50,23 @@ const closeMenu = () => {
   })
 }
 
+// Cambia a darkmode.
+const changeMode = () => {
+  const btnDark =document.getElementById("darkmode")
+  btnDark.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    if (btnDark.classList.contains("fa-moon")) {
+      btnDark.classList.replace("fa-moon", "fa-sun")
+    } else {
+      btnDark.classList.replace("fa-sun", "fa-moon")
+    }
+  })
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
   loadComponent();
   openMenu();
   closeMenu();
+  changeMode();
 });
