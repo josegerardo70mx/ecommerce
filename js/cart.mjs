a381d991e61f6e42951783d30cf56107534b5467
Mesach Venegas <mesach.venegas@hotmail.com>
2022-10-16 18:08:57 -0700

    create: loader producto to cart from js

--- js/cart.mjs
+++ js/cart.mjs
@@ -1,3 +1,5 @@
+import {items} from './products.mjs'
+
 const cart = document.getElementById('cart')
 
 export const openCart = () => {
@@ -12,4 +14,45 @@ export const closeCart = () => {
     btnClose.addEventListener( 'click',e => {
         cart.classList.remove('open')
     })
+}
+
+export const cartFill = () => {
+    let itemCounter = 0;
+    let allItems = 0;
+    let subtotal = 0;
+    let counter = 1;
+
+    items.forEach(item => {
+        const cartContent = document.createElement('div')
+        cartContent.classList.add('to-buy')
+        cartContent.innerHTML = `
+        <!-- Producto cargado desde JS -->
+        <div class='cart-item-img'>
+            <img src='./${item.image}'>
+        </div>
+        <div class='cart-item'>
+            <!-- Detalles del producto. -->
+            <h4>${item.name}</h4>
+            <div class='cart-stock-item'>
+            <p>Stock: ${item.quantity}</p>
+            <hr>
+            <p class='item-price'>$${item.price.toFixed(2)}</p>
+            </div>
+            <p> Subtotal: <span class='cart-subtotal'>${subtotal.toFixed(2)}</span></p>
+            <div class='cart-add-erase'>
+            <div class='btn-add-erase'>
+                <button type='button'  id='cart-decrease'> - </button>
+                <span id='qty-items'>1 units</span>
+                <button type='button' id='cart-add'> + </butt>
+            </div>
+            <span class='btn-erase'>
+                <i class="fa-solid fa-trash"></i>
+            </span>
+            </di>
+        </div>
+    `
+        document.querySelector(".cart-fill").appendChild(cartContent)
+        counter++
+    })
+
 }
